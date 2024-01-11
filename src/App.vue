6f3e0d08ff38961d5f043b80a86485f50ef90391
LuisKevL <105244365+LuisKevL@users.noreply.github.com>
2024-01-10 19:03:17 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div id="app">
+    <div class="container">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <template>
+      <div>
+        <b-sidebar id="sidebar-1" title="Sidebar" shadow visible no-header-close >
+          <div class="px-3 py-2">
+            <p>Hola a tod@s</p>
+            <b-img
+              src="https://picsum.photos/500/500/?image=54"
+              fluid
+              thumbnail
+            ></b-img>
+          </div>
+        </b-sidebar>
+      </div>
+    </template>
+  </div>
+</template>
+<style scoped>
+.container {
+  padding-left: 0%;
+}
+</style>
