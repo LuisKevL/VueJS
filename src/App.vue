1106ba5d1bd3aab15ecd53dfd0521a64afbfa6ec
LuisKevL <20213tn118@utez.edu.mx>
2024-01-12 20:42:28 -0600

    Navbar y sidebar algo mal hechos jjaja

--- src/App.vue
+++ src/App.vue
@@ -4,58 +4,55 @@ import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <div id="app">
-    <div class="container">
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-    <template>
-      <div>
-        <b-sidebar id="sidebar-1" title="Sidebar" shadow visible no-header-close >
+  <div id="app" class="container-fluid p-0">
+    <div class="row m-0" style="height: 100vh;">
+      <div class="col-md-3 p-0">
+        <b-sidebar id="sidebar-1" title="Bienvenidos..." shadow visible no-header-close>
           <div class="px-3 py-2">
-            <p>Hola a tod@s</p>
-            <b-img
-              src="https://picsum.photos/500/500/?image=54"
-              fluid
-              thumbnail
-            ></b-img>
+            <p>Hola, esta es la primer entrega del layout de la Unidad 1 - Desarrollo de layout</p>
+            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
           </div>
         </b-sidebar>
       </div>
-    </template>
+
+      <div class="col-md-9 p-0 ">
+        <b-navbar toggleable="lg" type="dark" variant="info" style="margin-left: -21px;">
+          <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item href="#">Link</b-nav-item>
+              <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav>
+              <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+              </b-nav-item-dropdown>
+
+              <b-nav-item-dropdown right>
+                <template #button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+        <div style="padding: 10px; margin-left: -20px;">
+          <b-card title="1er Tarea" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
+            tag="article" style="max-width: 20rem;" class="mb-2">
+            <b-card-text>
+              Hola, esta es la primer entrega del layout de la Unidad 1 - Desarrollo de layout </b-card-text>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-<style scoped>
-.container {
-  padding-left: 0%;
-}
-</style>
