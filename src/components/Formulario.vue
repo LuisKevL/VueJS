ff58f9c9590f945b568e173e99bb2c6c98651472
LuisKevL <20213tn118@utez.edu.mx>
2024-01-24 22:34:44 -0600

    form

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -14,26 +14,27 @@
         <input id="name" style="margin-left: 10px;" v-model="name" type="text" name="name" required>
       </p>
       <p>
-        <label for="direction">Dirección</label>
+        <label for="direction">Dirección (CP, Calle, número, Ciudad)</label>
         <input id="direction" style="margin-left: 10px;" v-model="direction" type="text" name="direction" required>
       </p>
 
       <p>
-        <label for="age">Fecha de nacimiento (menor de 18 años)</label>
+        <label for="age">Fecha de nacimiento</label>
         <input id="age" style="margin-left: 10px;" v-model="age" type="date" name="age" :max="maxDate()" required>
       </p>
       <p>
         <label for="correo">Correo electrónico</label>
-        <input id="correo" style="margin-left: 10px;" v-model="correo" type="email" name="correo" required>
+        <input id="correo" style="margin-left: 10px;" v-model="correo" type="email" name="correo"
+          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" required>
       </p>
 
       <p>
-        <label for="tel">Número de teléfono</label>
+        <label for="tel">Número telefónico</label>
         <input id="tel" style="margin-left: 10px;" v-model="tel" type="tel" name="tel" pattern="[0-9]{10}" required>
       </p>
 
       <p>
-        <label for="img">Subir fotografía (menos de 3MB)</label>
+        <label for="img">Fotografía (PNG)</label>
         <input id="img" style="margin-left: 10px;" type="file" accept="image/png" @change="handleImageUpload" required>
       </p>
 
@@ -61,8 +62,6 @@ const img = ref(null);
 const maxDate = () => {
   const today = new Date();
   today.setFullYear(today.getFullYear() - 18);
-
-  // Formatear la fecha actual para el atributo 'max' del input de fecha
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, "0");
   const day = String(today.getDate()).padStart(2, "0");
@@ -74,13 +73,13 @@ const checkForm = () => {
   errors.value = [];
 
   if (!name.value || !/^[a-zA-Z]+ [a-zA-Z]+( [a-zA-Z]+)?$/.test(name.value.trim())) {
-    errors.value.push("Nombre completo inválido. Ingrese nombre y al menos un apellido.");
+    errors.value.push("Nombre mal escrito. Ingrese nombre y al menos un apellido.");
   }
   if (!age.value) {
     errors.value.push("Edad requerida.");
   }
-  if (!direction.value) {
-    errors.value.push("Dirección requerida");
+  if (!direction.value || !/^\d{5} [a-zA-ZáéíóúüÁÉÍÓÚÜ\s]+ \d+( [a-zA-ZáéíóúüÁÉÍÓÚÜ\s]+)?$/.test(direction.value.trim())) {
+    errors.value.push("Dirección mal escrita. CP, calle, número y ciudad.");
   }
   if (!correo.value) {
     errors.value.push("Correo requerido");
@@ -89,23 +88,20 @@ const checkForm = () => {
     errors.value.push("Teléfono requerido");
   }
   if (!img.value) {
-    errors.value.push("Fotografía requerida");
+    errors.value.push("Foto requerida");
   }
 
   if (errors.value.length === 0) {
-    // Realizar alguna lógica adicional si es necesario
-
-    // Redirigir al usuario a otra página
     window.location.href = "https://vuejs.org/";
   }
 };
 
 const handleImageUpload = (event) => {
   const file = event.target.files[0];
-  const maxSize = 3 * 1024 * 1024; // 3MB
+  const maxSize = 3 * 1024 * 1024;
 
   if (file && file.size > maxSize) {
-    errors.value.push("La fotografía debe ser menor a 3MB");
+    errors.value.push("Foto menor a 3MB");
   } else {
     img.value = file;
   }
